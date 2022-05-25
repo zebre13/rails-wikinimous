38107e7709d59885e00fec1f3e4f6c53644b8408
Boris <95616326+zebre13@users.noreply.github.com>
2022-05-25 23:10:13 +0200

    show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,17 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def edit
@@ -20,5 +25,11 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
