b1db5d4bb17111ca4be4e86a8266d6a0b04612e7
Boris <95616326+zebre13@users.noreply.github.com>
2022-05-25 22:53:52 +0200

    index

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
