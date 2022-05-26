c878e69d5caaca221bbcc6b5465bc87cb4b553aa
Boris <95616326+zebre13@users.noreply.github.com>
2022-05-26 13:15:35 +0200

    backend working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
   end
+
+    def show
+    end
 
   def new
     @article = Article.new
@@ -10,24 +14,34 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
-  end
-
-  def show
-    @article = Article.find(params[:id])
+    if @article.save
+      redirect_to article_path(@article), notice: 'Article was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path, notice: 'A rticle was successfully destroyed.'
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :content)
